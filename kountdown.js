b8d59f5cd4c2da7925b3466781ee976d1e211598
kelvin.biffi <kelvin.biffi@pmweb.com.br>
2019-11-08 00:51:25 -0300

    Criado lógica e estrutura do countdown, adicionado exemplos, criado arquivo de estilo

--- kountdown.js
+++ kountdown.js
@@ -11,11 +11,32 @@ var Kountdown;
    * 
    * @return {void}
    */
-  var Kountdown = function (_element, _date, _position, _style) {
+  Kountdown = function (_element, _date, _position, _style) {
+    if (!_element) {
+      console.error('The _element must be set');
+      return;
+    }
+    if (!_date) {
+      console.error('The _date must be set as String or Date format');
+      return;
+    }
     this.element = (_element ? _element : null);
     this.date = (_date ? _date : null);
     this.position = (_position ? this.positions[_position] : this.positions['BEGIN']);
     this.style = (_style ? _style : null);
+    this.watcher = false;
+  };
+
+  /**
+   * Destroy Kountdown element
+   * 
+   * @return {Void}
+   */
+  Kountdown.prototype.destroy = function () {
+    if (this.watcher) {
+      clearInterval(this.watcher);
+    }
+    this.element.parentElement.removeChild(this.element);
   };
 
   /**
@@ -39,15 +60,122 @@ var Kountdown;
   /**
    * Check date format
    * 
-   * @return {Date|Boolean}
+   * @return {Boolean}
    */
   Kountdown.prototype.isDateValid = function () {
     if (typeof this.date === 'string' && this.regex.date.test(this.date)) {
-        return new Date(this.date);
+      this.date = new Date(this.date);
+      return true;
     } else if (this.date instanceof Date && Object.prototype.toString.call(this.date) === '[object Date]') {
-      return this.date;
+      return true;
     }
+    console.error('The date must be a Valid Date Object or a String Date YYYY-MM-DDTHH:MM:SS', this.element);
     return false;
+  };
+
+  /**
+   * Check callback function
+   * 
+   * @param {Function|String} callback Function that run after each down second
+   * 
+   * @return {Boolean:Function}
+   */
+  Kountdown.prototype.isCallbackValid = function (callback) {
+    if (!callback) return "No callback function";
+    if (typeof callback === 'function') {
+      return callback;
+    } else if (typeof callback === 'string' && window[callback] && typeof window[callback] === 'function') {
+      return window[callback];
+    }
+    console.error('Fatal error: Callback must be a Function or String function name from window', this.element);
+    return false;
+  };
+
+  /**
+   * Create the timer structure
+   * 
+   * @return {String}
+   */
+  Kountdown.prototype.timerStructure = function () {
+    var kid = 'kountdown' + new Date().getTime() + Math.floor((Math.random() * 1000) + 1);
+    this.element.insertAdjacentHTML(
+      this.position, 
+      '<div id="' + kid + '">' +
+      '  <div class="time">' +
+      '    <div class="day">' +
+      '      <span>00</span>' +
+      '      <small>Days</small>' +
+      '    </div>' +
+      '    <span>:</span>' +
+      '    <div class="hour">' +
+      '      <span>00</span>' +
+      '      <small>Hours</small>' +
+      '    </div>' +
+      '    <span>:</span>' +
+      '    <div class="minute">' +
+      '      <span>00</span>' +
+      '      <small>Minutes</small>' +
+      '    </div>' +
+      '    <span>:</span>' +
+      '    <div class="second">' +
+      '      <span>00</span>' +
+      '      <small>Seconds</small>' +
+      '    </div>' +
+      '  </div>' +
+      '</div>'
+    );
+
+    return kid;
+  };
+
+  /**
+   * Handle the timer countdown and timewatch
+   * 
+   * @param {String} id Id of the current timer
+   * @param {Function|String} callback Callback to each decrement second iteration
+   * 
+   * @return {void}
+   */
+  Kountdown.prototype.handleTimer = function (id, callback) {
+    var timerElements = {
+      day: document.querySelector('#' + id + ' .day span'),
+      dayLabel: document.querySelector('#' + id + ' .day small'),
+      hour: document.querySelector('#' + id + ' .hour span'),
+      hourLabel: document.querySelector('#' + id + ' .hour small'),
+      minute: document.querySelector('#' + id + ' .minute span'),
+      minuteLabel: document.querySelector('#' + id + ' .minute small'),
+      second: document.querySelector('#' + id + ' .second span'),
+      secondLabel: document.querySelector('#' + id + ' .second small')
+    }
+
+    var now = new Date();
+    var kountdownObject = this;
+    this.watcher = setInterval(function () {
+      setTimeout(function () {
+        now = new Date();
+        let res = Math.abs(now - kountdownObject.date) / 1000;
+        var time = {
+          kountdown: kountdownObject,
+          day: Math.floor(res / 86400),
+          hour: Math.floor(res / 3600) % 24,
+          minutes: Math.floor(res / 60) % 60,
+          seconds: Math.floor(res % 60)
+        };
+        timerElements.day.innerText = (time.day < 10 ? '0'+time.day : time.day);
+        timerElements.dayLabel.innerText = (time.day > 1 ? 'days' : 'day');
+        timerElements.hour.innerText = (time.hour < 10 ? '0'+time.hour : time.hour);
+        timerElements.hourLabel.innerText = (time.hour > 1 ? 'hours' : 'hour');
+        timerElements.minute.innerText = (time.minutes < 10 ? '0'+time.minutes : time.minutes);
+        timerElements.minuteLabel.innerText = (time.minutes > 1 ? 'minutes' : 'minute');
+        timerElements.second.innerText = (time.seconds < 10 ? '0'+time.seconds : time.seconds);
+        timerElements.secondLabel.innerText = (time.seconds > 1 ? 'seconds' : 'second');
+
+        if (callback && typeof callback === 'function') {
+          callback(time);
+        }
+      }, 300);
+    }, 1000);
+
   };
 
   /**
@@ -58,10 +186,13 @@ var Kountdown;
    * @return {void}
    */
   Kountdown.prototype.create = function (callback) {
-    if (this.isDateValid()) {
-      console.log('Create the Kountdown')
-    } else {
-      console.error('The date must be a Valid Date Object or a String Date YYYY-MM-DDTHH:MM:SS', this.element);
+    callback = this.isCallbackValid(callback);
+    console.log(callback);
+    if (this.isDateValid() && callback) {
+      console.log('Create the Kountdown');
+
+      var kid = this.timerStructure();
+      this.handleTimer(kid, callback);
     }
   };
 
@@ -71,15 +202,11 @@ var Kountdown;
   window.addEventListener('DOMContentLoaded',function(){
     var dc = [].slice.call(document.querySelectorAll('[data-kountdown]'));
     dc.forEach(function (dce) {
-      try {
-        kd = dce.dataset.kountdown.split('|');
-        if (kd && kd.length >= 1) {
-            new Kountdown(dce, kd[0], kd[1] ? kd[1] : null).create(kd[2] ? kd[2] : null);
-        } else {
-          console.error('Check if the dataset kountdown exists and is pattern formated');
-        }
-      } catch (error) {
-        console.error('Fatal error: Check if the dataset kountdown exists and is pattern formated');
+      kd = dce.dataset.kountdown.split('|');
+      if (kd && kd.length >= 1) {
+          new Kountdown(dce, kd[0], kd[1] ? kd[1] : null).create(kd[2] ? kd[2] : null);
+      } else {
+        console.error('Check if the dataset kountdown exists and is pattern formated');
       }
     });
   });
